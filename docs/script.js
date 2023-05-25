d02a48c93c905521148ee851347b370d12d0579e
AndreyAfanacyev <andreyafan20022202@gmail.com>
2023-05-25 12:13:10 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -21,3 +21,32 @@ buttons.forEach(button => {
     }
   });
 });
+document.addEventListener('DOMContentLoaded', function() {
+  // Получаем все кнопки с классом "section-button"
+  const buttons = document.querySelectorAll('.section-button');
+
+  // Для каждой кнопки добавляем обработчик события "click"
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Получаем значение атрибута "data-section" кнопки
+      const sectionId = button.getAttribute('data-section');
+
+      // Получаем соответствующий раздел по его id
+      const section = document.getElementById(sectionId);
+
+      // Проверяем, есть ли у раздела класс "active"
+      const isActive = section.classList.contains('active');
+
+      // Если раздел уже активен, то скрываем его, иначе отображаем
+      if (isActive) {
+        section.classList.remove('active');
+      } else {
+        section.classList.add('active');
+      }
+    });
+  });
+});
+
+
+
+
